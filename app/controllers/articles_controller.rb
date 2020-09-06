62951205837ed10f0f877edc1336367bcbf58f8e
zhafir <zzzhafir75@gmail.com>
2020-09-06 19:13:18 +0200

    Add edit and delete posts functionality
    Move form to its own partial template. Add flash messages for each significant
    action. Set root to route to index.

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -13,10 +13,34 @@ class ArticlesController < ApplicationController
     @article = Article.new(article_params)
     @article.save
 
+    flash.notice = "Article #{@article.title} created!"
+
     redirect_to article_path(@article)
   end
 
   def show
     @article = Article.find(params[:id])
+  end
+
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def update
+    @article = Article.find(params[:id])
+    @article.update(article_params)
+
+    flash.notice = "Article #{@article.title} updated!"
+
+    redirect_to article_path(@article)
+  end
+
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+
+    flash.notice = "Article #{@article.title} deleted!"
+
+    redirect_to action: "index"
   end
 end
